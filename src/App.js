9649244c4c03619ac6e4b4edf4ac7c13a682d2e7
s2zxc <s2zxc@hanmail.net>
2024-06-27 17:25:52 +0900

    add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React from "react";
+import HomePage from "./Routerrs/HomePage";
+import Trailer from "./Routerrs/Trailer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path={`${process.env.PUBLIC_URL}/`} element={<HomePage/>}></Route>
+        <Route path={`${process.env.PUBLIC_URL}/Trailer/:id`} element={<Trailer/>}></Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/Routerrs/HomePage.js
@@ -0,0 +1,76 @@
+import axios from "axios"
+import { useEffect, useState } from "react";
+import HomePageMovie from "../components/HomePageMovie";
+
+let data = [];
+
+const HomePage = () => {
+    const movieApiKey = "667564e273e374e6294f8e7794ae8062";
+    const tokenKey = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2Njc1NjRlMjczZTM3NGU2Mjk0ZjhlNzc5NGFlODA2MiIsIm5iZiI6MTcxOTM5MDczMC4xMzg5MDIsInN1YiI6IjY2N2JkMTBjMzIyZmFmOThmN2JhZjYwMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jXc-mMqsnmAbR77PQA_xykkgO8YKPMoWiwjpNjvp1NI";
+
+    const [movieList, setMovieList] = useState([]);
+    const [curDate, setCurDate] = useState(new Date);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    const fetchMovie = async () => {
+        const language = "ko-KR";
+        try {
+            // 상태 초기화
+            setError(null);
+            setMovieList([]);
+            setLoading(true);
+
+            const movieRes = await axios({ // 영화 기본정보세팅
+                headers: {
+                    accept: "application/json",
+                    Authorization: `Bearer ${tokenKey}`
+                },
+                baseURL: `https://api.themoviedb.org/3/discover/movie?`,
+                method: 'get',
+                params: {
+                    api_key: movieApiKey,
+                    include_adult: true,
+                    include_video: true,
+                    language: language,
+                    primary_release_year: curDate
+
+                },
+                proxy: {
+                    port: 3000
+                }
+            });
+            setMovieList(movieRes.data.results);
+            console.log(movieRes.data.results)
+            console.log(movieRes);
+
+
+        } catch (e) {
+            setError(e);
+        }
+        setLoading(false);
+
+    };
+
+
+
+    useEffect(() => {
+        fetchMovie();
+    }, [])
+
+    return (
+        <div>{loading ? <h1>Loding..</h1> : (movieList.map((e) =>
+            <HomePageMovie
+                id={e.id} // 영화 id값 추후 트레일러 불러올때 사용예정
+                key={e.id}
+                title={e.title} // 제목
+                overview={e.overview} // 상세설명
+                img={e.poster_path} // 포스터 이미지
+                average={e.vote_average} // 평점
+                releaseDate={e.release_date} // 상영일
+            />
+        ))}</div>
+    )
+}
+
+export default HomePage;
